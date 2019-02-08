6c859043572336473816b1f3c0273df3460f804c
FelixvL <digitaleservice@gmail.com>
2019-02-08 14:05:23 +0100

    eerste commit

---
+++ src/main/java/com/voorbeeld/datatour/DatatourApplication.java
@@ -0,0 +1,14 @@
+package com.voorbeeld.datatour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatatourApplication.class, args);
+	}
+
+}
+
